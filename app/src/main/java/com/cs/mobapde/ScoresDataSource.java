d4100d5e94b4b8e40a58ca8188551e6e10759dfe
Jan Joseph Abutog <jan_abutog@dlsu.edu.ph>
2017-03-26 23:24:41 +0800

    ScoresDataSource WIP (queryAllScores not done)

--- app/src/main/java/com/cs/mobapde/ScoresDataSource.java
+++ app/src/main/java/com/cs/mobapde/ScoresDataSource.java
@@ -1,8 +1,12 @@
 package com.cs.mobapde;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
 
 /**
  * Created by testAcc on 3/26/2017.
@@ -12,7 +16,7 @@ public class ScoresDataSource {
 
     private SQLiteDatabase database;
     private DatabaseHelper dbHelper;
-    private String[] allColumns = { DatabaseHelper.SCORE_ID, DatabaseHelper.COLUMN_SCORE};
+    private String[] allColumns = { DatabaseHelper.COLUMN_SCORE};
 
     public ScoresDataSource(Context context) {dbHelper = new DatabaseHelper(context); }
 
@@ -22,5 +26,48 @@ public class ScoresDataSource {
 
     public void close() {
         dbHelper.close();
+    }
+
+
+    public Scores addScore(int score){
+        ContentValues values = new ContentValues();
+
+        values.put(DatabaseHelper.COLUMN_SCORE, score);
+        long insertId = database.insert(DatabaseHelper.TABLE_SCORES,null,values);
+
+        Cursor cursor = database.query(DatabaseHelper.TABLE_SCORES,allColumns,
+                DatabaseHelper.SCORE_ID + " = " +insertId, null, null, null, null);
+
+        cursor.moveToFirst();
+        Scores newScore = cursorToScore(cursor);
+        cursor.close();
+
+        return  newScore;
+
+    }
+
+    public void deleteScore(Scores score){
+        int id = score.getId();
+        System.out.println("deleted score id: " + id);
+        database.delete(DatabaseHelper.TABLE_SCORES,DatabaseHelper.SCORE_ID + " = "
+                + id, null );
+
+    }
+
+    public List<Scores> queryAllScores(){
+
+
+    }
+
+
+    private Scores cursorToScore(Cursor cursor){
+        Scores score = new Scores();
+        score.setId(cursor.getInt(0));
+        score.setScore(cursor.getInt(1));
+
+        System.out.println(cursor.getString(1));
+
+        return score;
+
     }
 }
